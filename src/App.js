84985ba47899b79cabb7cd14e165c29e4ebb4d77
Myo Thu Ko <myothuko@ucsy.edu.mm>
2022-01-07 19:34:35 +0630

    add collection views

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ShopPage from "./components/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,16 @@
+import CollectionItem from "../collection-item/collection-item.component";
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1>{title}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
+        ))}
+    </div>
+  </div>
+);
+export default CollectionPreview;
